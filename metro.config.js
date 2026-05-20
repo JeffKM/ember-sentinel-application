ca23619fe7e88199f1f31d5456e05acbabd3bafe
JeffKM <jeffkm@inha.edu>
2026-05-21 01:44:27 +0900

    🐛 fix: Metro blockList에서 자기 자신(ember-sentinel) 제거
    ember-sentinel/.* 패턴이 blockList에 포함되어 프로젝트 자체 모듈을
    차단하던 버그 수정

--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,7 @@ const config = getDefaultConfig(__dirname);
 // 새 아키텍처 문제 우회
 config.resolver.unstable_enablePackageExports = false;
 
-// 다른 프로젝트 디렉토리 제외
+// 다른 프로젝트 디렉토리 제외 (자기 자신은 제외하지 않도록 주의)
 config.resolver.blockList = [
   /CoTePractice\/.*/,
   /INHA_OSAP_002_T6\/.*/,
@@ -14,7 +14,6 @@ config.resolver.blockList = [
   /naraka-chat\/.*/,
   /naraka-mobile\/.*/,
   /zagabi\/.*/,
-  /ember-sentinel\/.*/,
 ];
 
 module.exports = config;
